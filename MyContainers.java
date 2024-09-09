0e79a1876479c3a568fbae378d8007c30f24563b
kkiyoi <30498702+kkiyoi@users.noreply.github.com>
2024-09-09 11:25:48 -0700

    Update MyContainers.java

--- MyContainers.java
+++ MyContainers.java
@@ -1,12 +1,8 @@
 import javax.swing.*;
 import java.awt.event.*;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.ImageIcon;
 import java.awt.*;
 import java.awt.Color;
-import javax.swing.border.Border;
-import javax.swing.BorderFactory;
+
 
 public class MyContainers {
   MyContainers() {
